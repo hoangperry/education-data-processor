30fa509eb1f739c6b761adb24a6dbac20c64f5df
hoangperry <prodai9573@gmail.com>
2022-01-06 03:47:00 +0700

    add quote parse for mongodb string connection

--- utils/database.py
+++ utils/database.py
@@ -1,3 +1,6 @@
+import re
+import urllib.parse
+
 from pymongo import MongoClient
 from utils.environments import create_environments
 
@@ -5,17 +8,32 @@ env = create_environments()
 
 
 def create_mongo_url_connection(username=None, password=None, host=None, port=None):
-    username = env.database_username if username is None else username
-    password = env.database_password if password is None else password
-    host = env.database_host if host is None else host
-    port = env.database_port if port is None else port
-
+    username = urllib.parse.quote(env.database_username if username is None else username)
+    password = urllib.parse.quote(env.database_password if password is None else password)
+    host = urllib.parse.quote(env.database_host if host is None else host)
+    port = urllib.parse.quote(env.database_port if port is None else port)
+    print(f'mongodb://{username}:{password}@{host}:{port}/')
     return f'mongodb://{username}:{password}@{host}:{port}/'
 
 
 def create_connection(connection_url=None):
     connection_url = create_mongo_url_connection() if connection_url is None else connection_url
     return MongoClient(connection_url)
+
+
+def get_uni_by_institution(institution, database_name=None, client_connection=None):
+    database_name = env.database_name if database_name is None else database_name
+    if client_connection is None:
+        client_connection = create_connection()
+        connection_already_existed = False
+    else:
+        connection_already_existed = True
+    uni = client_connection[database_name][env.database_university_collection].find_one(
+        {'institution': re.compile(institution, re.IGNORECASE)}
+    )
+    if not connection_already_existed:
+        client_connection.close()
+    return uni
 
 
 def push_new_document(document, collection_name, database_name=None, client_connection=None):
